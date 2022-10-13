aa050124f5c253081d5dd5413a6f3ff5ffc1bdc1
plhome <plhome@yandex.ru>
2022-10-13 15:08:23 +0300

    after eslint

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 import { promiseReduce } from "./js/task1";
 
 var fn1 = () => {
-    console.log('fn1');
-    return Promise.resolve(1);
-  }
-  
-  var fn2 = () => new Promise(resolve => {
-    console.log('fn2');
+  console.log("fn1");
+  return Promise.resolve(1);
+};
+
+var fn2 = () =>
+  new Promise((resolve) => {
+    console.log("fn2");
     setTimeout(() => resolve(2), 1000);
   });
 
-  var fn3 = () => new Promise(resolve => {
-    console.log('fn3');
+var fn3 = () =>
+  new Promise((resolve) => {
+    console.log("fn3");
     setTimeout(() => resolve(5), 1000);
   });
 
 promiseReduce(
-    [fn1, fn2, fn3],
-    function (memo, value) {
-      console.log('reduce');
-      return memo * value;
-    },
-    1
-  ).then(console.log)
+  [fn1, fn2, fn3],
+  function (memo, value) {
+    console.log("reduce");
+    return memo * value;
+  },
+  1
+).then(console.log);
--- src/js/task1.js
+++ src/js/task1.js
@@ -1,8 +1,7 @@
-
 export function promiseReduce(asyncFunctions, reduce, initialValue) {
-  return asyncFunctions.reduce( (p, fn) => {
-    return p.then( async pVal => {
+  return asyncFunctions.reduce((p, fn) => {
+    return p.then(async (pVal) => {
       return reduce(pVal, await fn());
-    })  
+    });
   }, Promise.resolve(initialValue));
 }
