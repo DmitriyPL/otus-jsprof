1cefd58a61682efe67f33adb7f33df7885aef9b2
plhome <plhome@yandex.ru>
2022-10-13 15:07:37 +0300

    async function done

---
+++ src/js/task1.js
@@ -0,0 +1,8 @@
+
+export function promiseReduce(asyncFunctions, reduce, initialValue) {
+  return asyncFunctions.reduce( (p, fn) => {
+    return p.then( async pVal => {
+      return reduce(pVal, await fn());
+    })  
+  }, Promise.resolve(initialValue));
+}
