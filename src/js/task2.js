f2712352fe29a8f6ae22387ad9322a8d41f2b2ee
plhome <plhome@yandex.ru>
2022-10-14 17:37:03 +0300

    after eslint

--- src/js/task2.js
+++ src/js/task2.js
@@ -1,8 +1,8 @@
 export function sum(...args) {
-    if (args.length === sum.prototype.calls) {
-      return args.reduce((prev, curr) => prev + curr, 0);
-    } else {
-      sum.prototype.calls = args.length;
-      return sum.bind(this, ...args);
-    }
+  if (args.length === sum.prototype.calls) {
+    return args.reduce((prev, curr) => prev + curr, 0);
+  } else {
+    sum.prototype.calls = args.length;
+    return sum.bind(this, ...args);
   }
+}
