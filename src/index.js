8da6c797dfee51f507da1f3e0208005963dbb6c6
plhome <plhome@yandex.ru>
2022-10-21 21:22:22 +0300

    add getPath impl

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import { getPath } from "./js/getPath";
+
+const arrEl = [...document.querySelectorAll("p")]
+    .filter(a => a.textContent.includes("Тест3"))
+
+const testEl =  arrEl[0];
+console.log(testEl);
+
+const finedPath = getPath(testEl);
+console.log(finedPath);
+
+const elChek = document.querySelector(finedPath);
+console.log(elChek);
+
---
+++ src/js/getPath.js
@@ -0,0 +1,43 @@
+
+export function getPath(el){
+    
+    if (!(el instanceof Element))
+            return "";
+
+    const path = [];
+
+    while (el.localName != "html") {
+
+        const selector = el.localName;
+        
+        if (el.id) {
+            
+            selector += '#' + el.id;
+            path.unshift(selector);
+            
+            break;
+
+        } else {
+
+            let sib = el;
+            let nth = 1;
+
+            while (sib = sib.previousElementSibling) {
+                if (sib.localName == selector){
+                    nth++;
+                }
+            }
+
+            if (nth != 1){
+                selector += `:nth-of-type(${nth})`;
+            }
+                
+        }
+
+        path.unshift(selector);
+
+        el = el.parentNode;
+    }
+
+    return path.join(" > ");
+}
