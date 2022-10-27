4aae4d929ea8e1a911e6a6bdb17ab0942be07338
plhome <plhome@yandex.ru>
2022-10-27 21:48:56 +0300

    add function tree()

---
+++ src/js/tree.js
@@ -0,0 +1,46 @@
+
+import { promises} from 'node:fs';
+import path from 'node:path';
+import { argv } from 'node:process';
+
+export async function tree() {
+
+    const startPath = argv[2];
+
+    let fsMap = {
+        files: [],
+        folders: [],
+    }
+
+    try{
+        await promises.access(startPath);
+    } catch (err) {
+        console.log(`${startPath} ${err ? 'does not exist' : 'exists'}`);
+        return fsMap;
+    }
+
+    fsMap.folders.push(startPath);
+    await getSubDir(startPath, fsMap);        
+
+    return fsMap;
+}
+
+async function getSubDir(startPath, fsMap) {
+
+    const folders = await promises.readdir(startPath, {withFileTypes: true});
+
+    for (let folder of folders) {
+
+        let newPath = path.join(startPath, folder.name);
+
+        if (folder.isDirectory()){
+            fsMap.folders.push(newPath);
+            await getSubDir(newPath, fsMap);
+        } else {
+            fsMap.files.push(newPath);
+        }
+
+    };
+}
+
+console.log(await tree());
